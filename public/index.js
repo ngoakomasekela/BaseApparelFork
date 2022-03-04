d1d2723d7301fc7ff44390d66dc0c2c1c7606cec
ngoako <36847864+ngoako@users.noreply.github.com>
2022-03-04 10:12:28 +0200

    first commit

---
+++ public/index.js
@@ -0,0 +1,2 @@
+document.getElementById("span-one").innerHTML="We're";
+document.getElementById("paragraph").innerHTML="Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals.";
